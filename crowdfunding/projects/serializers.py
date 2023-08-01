a986b3c798415448886716e6dfc31b251d7c7c92
Pauline Segi <psegi@tritium.com.au>
2023-08-02 09:50:56 +1000

    changed owner to supporter in project serializers and views files for pledges

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from django.apps import apps
 
 class PledgeSerializer(serializers.ModelSerializer):
+    supporter = serializers.ReadOnlyField(source='supporter.id')
+    
     class Meta:
         model = apps.get_model('projects.Pledge')
         fields = '__all__'
+
 
 class ProjectSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.id')
@@ -12,6 +15,7 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = apps.get_model('projects.Project')
         fields ='__all__'
+
 
 class ProjectDetailSerializer(ProjectSerializer):
     pledges = PledgeSerializer(many=True, read_only=True)
